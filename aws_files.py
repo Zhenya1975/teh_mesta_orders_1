12167a2ea5787532261c0be88d785e2ea1d6e4f9
Zhenya <57045129+Zhenya1975@users.noreply.github.com>
2022-04-01 15:44:08 +0000

    победил проблему хранения больших файлов

--- aws_files.py
+++ aws_files.py
@@ -23,8 +23,19 @@ def delete_file():
   except:
     pass
 
+def upload_file(file_path, file_name):
+  s3.upload_file(
+      Filename=file_path,
+      Bucket="pydatabucket",
+      Key=file_name,
+  )
+
+file_path = "temp_files/df.csv"
+file_name = "full_eo_list_actual.csv"
+# upload_file(file_path, file_name)
+
 # получаем файл
-maintanance_jobs_df = pd.read_csv(get_file("maintanance_jobs_df.csv"))
+# maintanance_jobs_df = pd.read_csv(get_file("maintanance_jobs_df.csv"))
 # удаляем файл
 # delete_file()
 # print(maintanance_jobs_df.info())
